bf92f61b61b24ce2f1e1b9787a4727a7dc06c814
keisuke_goto <keisuke_goto@Pro20150214008.procd-kama.local>
2020-06-16 10:44:48 +0900

    k

--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/PasswordAction.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/PasswordAction.java
@@ -30,7 +30,7 @@ public final class PasswordAction extends Action {
 	 * <p>
 	 * メイン画面アクションの初期設定を行う。
 	 * </p>
-	 * 
+	 *
 	 * 1.初期設定を行う。<br>
 	 * 　1-1.DB接続クラスのインスタンスを生成する。<br>
 	 * 　1-2.遷移先<br>
@@ -38,11 +38,11 @@ public final class PasswordAction extends Action {
 	 * 2.例外発生時の処理。<br>
 	 * 　2-1.IOExceptionをthrowする。<br>
 	 * <br>
-	 * 
+	 *
 	 * @throws IOException
 	 *             -
 	 */
-	public MainAction() throws IOException {
+	public PasswordAction() throws IOException {
 
 	}
 
@@ -50,7 +50,7 @@ public final class PasswordAction extends Action {
 	 * <p>
 	 * クリックされたボタンを判定し、遷移先情報を返却する。
 	 * </p>
-	 * 
+	 *
 	 * 1.メイン画面のアクションフォーム情報をインプットパラメータ.アクションフォームから取得する。<br>
 	 * <br>
 	 * 2.クリックされたボタンの名称をアクションフォームから取得する。<br>
@@ -102,28 +102,28 @@ public final class PasswordAction extends Action {
 	 * 　　クラス　：ActionMapping<br>
 	 * 　　メソッド：findForward(遷移先)<br>
 	 * <br>
-	 * 
+	 *
 	 * @param map
 	 *            アクションマッピング<br>
 	 *            frm アクションフォーム<br>
 	 *            request リクエスト情報<br>
 	 *            response レスポンス情報<br>
 	 * @return 遷移先情報
-	 * 
+	 *
 	 */
 	public ActionForward execute (ActionMapping map,ActionForm frm,HttpServletRequest request,HttpServletResponse response) {
 
-		
-		
+
+
 		// アクションフォームBeanより入力フォームのデータを取り出す処理
 		// フォーム情報をキャスト
 		MainForm mForm = (MainForm) frm;
-		
+
 		// フォームへ入力された情報をとりだす。
 		String syain_no = mForm.getSyain_no();
 		// クリックされたボタンの名称をアクションフォームから取得
 		String button = mForm.getButton();
-		
+
 		// 社員番号を整形
 		// 　　※桁数が4桁未満の場合は先頭から"0"埋め)<br>
 		if(syain_no.length() < 4) {
@@ -131,7 +131,7 @@ public final class PasswordAction extends Action {
 				syain_no = "0" + syain_no;
 			}
 		}
-		
+
 		// 社員名の取得
 		// 　　クラス　：DbAction<br>
 		// 　　メソッド：getSyainName()<br>
@@ -146,10 +146,10 @@ public final class PasswordAction extends Action {
 					clickBtnIn(mForm);
 					break;
 				case "taisya":
-					
+
 					break;
 				case "sansyou":
-					
+
 					break;
 				}
 			}
@@ -164,7 +164,7 @@ public final class PasswordAction extends Action {
 				mForm.setMessage("社員マスタに存在しない社員番号です。");
 				return (map.findForward("message"));
 			}
-			
+
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
@@ -177,7 +177,7 @@ public final class PasswordAction extends Action {
 		 * 　　　引数２：メイン画面アクションフォーム
 		 */
 		request.setAttribute("form", mForm);
-		
+
 		/* 7.戻り値を返却する。<br>
 		 * 　7-1.遷移先情報取得処理をコール。<br>
 		 * 　　クラス　：ActionMapping<br>
@@ -190,7 +190,7 @@ public final class PasswordAction extends Action {
 	 * <p>
 	 * 出社メソッド
 	 * </p>
-	 * 
+	 *
 	 * 1.出退時間を取得する。<br>
 	 * 　1-1.出退時間取得処理を呼び出し<br>
 	 * 　　クラス　：DbAction<br>
@@ -205,7 +205,7 @@ public final class PasswordAction extends Action {
 	 * 3.出社時間登録済みの場合<br>
 	 * 　3-1.メッセージ出力<br>
 	 * 　　"既に出社しています。"<br>
-	 * 
+	 *
 	 * @param form
 	 *            メイン画面アクションフォーム
 	 * @return 遷移先
@@ -215,15 +215,15 @@ public final class PasswordAction extends Action {
 		form.setTime_from(time_get());
 		// 出社時間が入力されていなかったら...
 		if (dba.getTimeFromTo(form)) {
-			
+
 			/*
 			String day = String.format("%tY年 %tm月 %td日", time_get().substring(0,8));
 				//	time_get().substring(0,8);
 			String time = time_get().substring(9);*/
-			
+
 			Date today = new Date();
 			SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日 HH:mm");
-			
+
 			dba.setTimeFrom(form);
 			form.setMessage(form.getSyain_name() + "　出社しました。");
 			form.setDtime(format.format(today));
@@ -242,7 +242,7 @@ public final class PasswordAction extends Action {
 	 * <p>
 	 * 退社メソッド
 	 * </p>
-	 * 
+	 *
 	 * 1.出退時間を取得する。<br>
 	 * 　1-1.出退時間取得処理を呼び出し<br>
 	 * 　　クラス　：DbAction<br>
@@ -257,7 +257,7 @@ public final class PasswordAction extends Action {
 	 * 　　メソッド：setTimeTo()<br>
 	 * 　3-2.メッセージ出力<br>
 	 * 　　"退社しました。"<br>
-	 * 
+	 *
 	 * @param form
 	 *            メイン画面アクションフォーム
 	 * @return 遷移先
@@ -280,7 +280,7 @@ public final class PasswordAction extends Action {
 
 	/**
 	 * 参照メソッド
-	 * 
+	 *
 	 * 1.勤怠情報取得メソッド<br>
 	 * 　クラス　：DbAction<br>
 	 * 　メソッド：getKintaiInfo()<br>
@@ -298,11 +298,11 @@ public final class PasswordAction extends Action {
 	 * @return 遷移先
 	 */
 	private String clickBtnView(MainForm form) {
-		
+
 		// 勤怠情報取得
 		boolean result = dba.getKintaiInfo(form);
-		
-		
+
+
 		if(result) {
 			return "view";
 		}
@@ -310,27 +310,27 @@ public final class PasswordAction extends Action {
 			System.out.println("勤怠情報がありません。");
 			return "message";
 		}
-		
+
 	}
 	public static String time_get() {
 		LocalDateTime date1 = LocalDateTime.now();
 		// 日付の取得
-		DateTimeFormatter dtformat1 = 
+		DateTimeFormatter dtformat1 =
 				DateTimeFormatter.ofPattern("yyyy年MM月dd日");
 		String day = dtformat1.format(date1);
 		// 時間の取得
-		DateTimeFormatter dtformat2 = 
+		DateTimeFormatter dtformat2 =
 				DateTimeFormatter.ofPattern("HHmm");
 		String time = dtformat2.format(date1);
-		
+
 		// 日付と時間の取得
-		DateTimeFormatter dtformat3 = 
+		DateTimeFormatter dtformat3 =
 				DateTimeFormatter.ofPattern("yyyyMMdd,HHmm");
 		String datetime = dtformat3.format(date1);
 		System.out.println(day);
 		System.out.println(time);
 		System.out.println(datetime);
-		
+
 		return datetime;
 	}
 }
