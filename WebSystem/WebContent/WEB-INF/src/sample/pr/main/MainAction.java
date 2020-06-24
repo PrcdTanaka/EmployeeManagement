3fb013b4228c8634e1c13edd2b23520ce7196aa6
prcd2020-kensyu <behk1240312@gn.iwasaki.ac.jp>
2020-06-24 13:09:17 +0900

    kikuchi 戻るボタンの適用

--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/MainAction.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/MainAction.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
