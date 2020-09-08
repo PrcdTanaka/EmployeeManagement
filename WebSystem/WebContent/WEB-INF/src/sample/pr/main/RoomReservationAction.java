35e9ad0bccebd4152796fd6157d5de845f02ccb7
ryunosuke_sunaga <ryunosuke_sunaga@Pro20150214001.procd-kama.local>
2020-09-08 13:21:12 +0900

    gotou

---
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/RoomReservationAction.java
@@ -0,0 +1,52 @@
+package sample.pr.main;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import sample.ap.DbAction;
+
+public final class RoomReservationAction extends Action{
+
+	// DB接続用オブジェクト
+	private DbAction dba = new DbAction();
+
+	// 遷移先
+	private String forward;
+
+	public RoomReservationAction() throws IOException {
+	}
+	String button;
+	public ActionForward execute (ActionMapping map,ActionForm frm,HttpServletRequest request,HttpServletResponse response) {
+
+		try {
+			request.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		ReservationForm rForm = (ReservationForm) frm;
+		HttpSession session = request.getSession();
+		LoginForm lForm = (LoginForm) session.getAttribute("form");
+		rForm.setEmployee_no(lForm.getEmployee_no());
+		forward="reservation";
+		String button=rForm.getButton();
+		try{
+			if(button.equals("戻る")){
+				forward="main";
+				session.removeAttribute("rForm");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		session.removeAttribute("rForm");
+		return map.findForward(forward);
+	}
+}
