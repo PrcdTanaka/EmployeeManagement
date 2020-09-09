b5a3da1e52a6030dc940bbd8c8c9d39b9b5bb83b
kosuke_eizumi <kosuke_eizumi@Pro20150214023.procd-kama.local>
2020-09-09 16:46:29 +0900

    aa

--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/KintaiMailAction.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/KintaiMailAction.java
@@ -14,7 +14,7 @@ import org.apache.struts.action.ActionMapping;
 
 import sample.ap.DbAction;
 
-public class KintaiMailAction extends Action{
+public class KintaiMailAction extends Action {
 	private DbAction dba = new DbAction();
 
 	// 遷移先
@@ -22,8 +22,11 @@ public class KintaiMailAction extends Action{
 
 	public KintaiMailAction() throws IOException {
 	}
+
 	String button;
-	public ActionForward execute (ActionMapping map,ActionForm frm,HttpServletRequest request,HttpServletResponse response) {
+
+	public ActionForward execute(ActionMapping map, ActionForm frm,
+			HttpServletRequest request, HttpServletResponse response) {
 		try {
 			request.setCharacterEncoding("utf-8");
 		} catch (UnsupportedEncodingException e) {
@@ -33,34 +36,35 @@ public class KintaiMailAction extends Action{
 		HttpSession session = request.getSession();
 		LoginForm lForm = (LoginForm) session.getAttribute("form");
 		form.setEmployee_no(lForm.getEmployee_no());
-		forward="kintaimail";
-		String button=form.getButton();
-		try{
-			if(button.equals("戻る")){
-				forward="KintaiMain";
+		forward = "kintaimail";
+		String button = form.getButton();
+		try {
+			if (button.equals("戻る")) {
+				forward = "KintaiMain";
 				session.removeAttribute("form");
 			}
-			if(button.equals("送信")){
-				if(form.getCC().equals("")||form.getSpotcode().equals("")||form.getDivision().equals("")||form.getSpan().equals("")
-						||form.getPtime().equals("")||form.getRemark().equals("")||form.getDepart().equals("")||form.getSpan2().equals(""))
-				{
+			if (button.equals("送信")) {
+				if (form.getCC().equals("") || form.getSpotcode().equals("")
+						|| form.getDivision().equals("")
+						|| form.getSpan().equals("")
+						|| form.getPtime().equals("")
+						|| form.getRemark().equals("")
+						|| form.getDepart().equals("")
+						|| form.getSpan2().equals("")) {
 					form.setMessage("必須項目を入力してください");
-					//JOptionPane.showMessageDialog(null,"必須項目を入力してください" );
-					forward="kintaimail";
-				}
-				else
-				{
-					forward="kintaimail";
+					// JOptionPane.showMessageDialog(null,"必須項目を入力してください" );
+					forward = "kintaimail";
+				} else {
+					forward = "kintaimail";
 					dba.setKintaiInfo(form);
 					session.setAttribute("form", form);
-					//JOptionPane.showMessageDialog(null,"送信しました");
+					// JOptionPane.showMessageDialog(null,"送信しました");
 				}
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		session.removeAttribute("form");
 		return map.findForward(forward);
 	}
 }
-
