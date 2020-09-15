ec7a0efcc53aff8be6239493449bce7a36cb4fd9
yuji_kiryu <yuji_kiryu@pro20190203009.procd-kama.local>
2020-09-15 18:05:47 +0900

    勤怠届作成画面(DB登録機能追加)

--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/KintaiNotificationAction.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/KintaiNotificationAction.java
@@ -5,16 +5,19 @@ import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import sample.ap.DbAction;
+
 public final class KintaiNotificationAction extends Action {
 
 	// DB接続用オブジェクト
-	//private DbAction dba = new DbAction();
+	private DbAction dba = new DbAction();
 
 	// 遷移先
 	private String forward;
@@ -32,51 +35,129 @@ public final class KintaiNotificationAction extends Action {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		// 1.ログイン画面のアクションフォーム情報をインプットパラメータ.アクションフォームから取得する。
-		// アクションフォームBeanより入力フォームのデータを取り出す処理
+
 		// フォーム情報をキャスト
 		KintaiNotificationForm KNForm = (KintaiNotificationForm) frm;
 
+		//セッションインスタンスをまず取得()
+		HttpSession session = request.getSession();
+
+		//ログインユーザーの社員番号と名前をセッションスコープから取得
+		LoginForm lForm = (LoginForm) session.getAttribute("form");
+		lForm.setEmployee_no(lForm.getEmployee_no());
 
 		//チェック要の項目を変数に格納する。
-		String employee_no = KNForm.getEmployee_no();
-		String petition_ymd = KNForm.getPetition_ymd();
+		String check_employee_no = request.getParameter("employee_no");
+		KNForm.setEmployee_no(check_employee_no);
+		String check_at_Startday = KNForm.getAttendance_startday();
+		String check_at_Endday = KNForm.getAttendance_endday();
+		String check_NotificationReason = KNForm.getNotification_reason();
 		String b=KNForm.getButton();
 		if(b.equals("戻る")){
 			forward ="main";
 		}
-		else if (employee_no.equals("")) {
+		else if (check_employee_no.equals("")) {
 			KNForm.setMessage("社員番号が空白になっています。");
 			forward = "kintaiNotification";
 		}
-		else if(petition_ymd.equals("")){
-			KNForm.setMessage("申請日が空白になっています。");
+		else if(check_at_Startday.equals("")){
+			KNForm.setMessage("対象日(開始)が空白になっています。");
+			forward = "kintaiNotification";
+		}else if(check_at_Endday.equals("")){
+			KNForm.setMessage("対象日(終了)が空白になっています。");
+			forward = "kintaiNotification";
+		}else if(check_NotificationReason.equals("")){
+			KNForm.setMessage("届出事由が空白になっています。");
 			forward = "kintaiNotification";
 		}
 		else{
+			KNForm.setMessage("エクセル出力しました。");
 			forward="kintaiNotification";
+
+			/* ここからは構想上のプログラム
+			 *
+			 * 社員番号と対象開始日と対象終了日と届出事由をDBから取得して、画面入力値と比較し
+			 * 完全一致ならUPDATE文、それ以外であればINSERT文でDBに登録する。
+			 */
+
+			//リクエストパラメーターを取得
+			String employee_no = request.getParameter("employee_no");
+			String syain_name = request.getParameter("syain_name");
+			String depart = request.getParameter("depart");
+			String petition_ymd = request.getParameter("petition_ymd");
+			String attendance_startday = request.getParameter("attendance_startday");
+			String attendance_endday = request.getParameter("attendance_endday");
+			String attendance_starttime = request.getParameter("attendance_starttime");
+			String attendance_endtime = request.getParameter("attendance_endtime");
+			String notification_reason = request.getParameter("notification_reason");
+			String vacation_division = request.getParameter("vacation_division");
+			String transfer_day = request.getParameter("transfer_day");
+			String sp_holiday_reason = request.getParameter("sp_holiday_reason");
+			String absenteeism_reason = request.getParameter("absenteeism_reason");
+			String reason = request.getParameter("reason");
+
+			/*確認するカラムをSELECTで確認する。
+			 * 確認カラム
+			 * 社員番号、対象開始日、対象終了日、届出事由
+			 *
+			 * StringBuffer sb = new StringBuffer();
+			 * sb.append(employee_no + attendance_startday + attendance_endday + notification_reason);
+			 *
+			 */
+			//セッターでインスタンスのフィールド変数を更新
+			KNForm.setEmployee_no(employee_no);
+			KNForm.setSyain_name(syain_name);
+			KNForm.setDepart(depart);
+			KNForm.setPetition_ymd(petition_ymd);
+			KNForm.setAttendance_startday(attendance_startday);
+			KNForm.setAttendance_endday(attendance_endday);
+			KNForm.setAttendance_starttime(attendance_starttime);
+			KNForm.setAttendance_endtime(attendance_endtime);
+			KNForm.setNotification_reason(notification_reason);
+			KNForm.setVacation_division(vacation_division);
+			KNForm.setReason(reason);
+
+			//休暇区分によって、入力不要なデータを消す。
+			switch(vacation_division){
+			//休暇区分が1(年次有給休暇/リフ休)の場合
+			case "1":
+				KNForm.setTransfer_day("");
+				KNForm.setSp_holiday_reason("");
+				KNForm.setAbsenteeism_reason("");
+				break;
+
+			//休暇区分が3(振替休暇)の場合
+			case "3":
+				KNForm.setTransfer_day(transfer_day);
+				KNForm.setSp_holiday_reason("");
+				KNForm.setAbsenteeism_reason("");
+				break;
+
+			//休暇区分が4(特別休暇)の場合
+			case "4":
+				KNForm.setTransfer_day("");
+				KNForm.setSp_holiday_reason(sp_holiday_reason);
+				KNForm.setAbsenteeism_reason("");
+				break;
+			case "5":
+				KNForm.setTransfer_day("");
+				KNForm.setSp_holiday_reason("");
+				KNForm.setAbsenteeism_reason(absenteeism_reason);
+			case "null":
+				KNForm.setTransfer_day("");
+				KNForm.setSp_holiday_reason("");
+				KNForm.setAbsenteeism_reason("");
+				break;
+			}
+
+			forward = saveInsert(KNForm);
+
 		}
-
-		/*
-		request.setAttribute("form", KNForm);
-
-
-		HttpSession session = request.getSession();
-
-		session.setAttribute("form", KNForm);
-
-		 */
-
-		/*if(forward.equals("login")){
-		//	JOptionPane.showMessageDialog(null,KNForm.getMessage());
-		}*/
-		/* 9.戻り値を返却する。<br>
-		 * 　9-1.遷移先情報取得処理をコール。<br>
-		 * 　　クラス　：ActionMapping<br>
-		 * 　　メソッド：findForward(遷移先)<br>
-		 */
-
-
 		return map.findForward(forward);
+	}
+
+	public String saveInsert(KintaiNotificationForm form){
+		dba.KintaiNotification_INSERT(form);
+		return "kintaiNotification";
 	}
 }
--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/KintaiNotificationForm.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/KintaiNotificationForm.java
@@ -71,292 +71,165 @@ public final class KintaiNotificationForm extends ActionForm {
 		button="";
 	}
 
-	/**
-	 * 社員番号取得処理。
-	 * <p>
-	 * 勤怠届作成画面アクションフォームから社員番号を取得する。
-	 * </p>
-	 *
-	 * @return 社員番号
-	 */
+/* 社員番号(employee_no) getter */
 	public String getEmployee_no() {
 		return employee_no;
 	}
 
-	/**
-	 * 社員番号設定処理。
-	 * <p>
-	 * ログイン画面アクションフォームに社員番号を設定する。
-	 * </p>
-	 *
-	 * @param employee_no 社員番号
-	 */
+/* 社員番号(employee_no) setter*/
 	public void setEmployee_no(String employee_no) {
 		this.employee_no = employee_no;
 	}
 
-	/**
-	 * 社員氏名取得処理。
-	 * <p>
-	 * ログイン画面アクションフォームから社員氏名を取得する。
-	 * </p>
-	 *
-	 * @return 社員氏名
-	 */
-	public String getSyain_name() {
+/* 社員氏名(syain_name) getter */
+	public String getSyain_name(){
 		return syain_name;
 	}
 
-	/**
-	 * 社員氏名設定処理。
-	 *
-	 * 勤怠届作成画面アクションフォームに社員氏名を設定する。
-	 *
-	 *
-	 * @param syain_name 社員氏名
-	 */
-	public void setEmployee_name(String syain_name) {
+/* 社員氏名(syain_name) setter */
+	public void setSyain_name(String syain_name){
 		this.syain_name = syain_name;
 	}
 
-	/**
-	 * @return depart
-	 */
+/* 所属部門(depart) getter */
 	public String getDepart() {
-		return depart;
+	return depart;
 	}
 
-	/**
-	 * @param depart セットする depart
-	 */
+/* 所属部門(depart) setter */
 	public void setDepart(String depart) {
-
-		switch(depart){
-		case "1":
-			this.depart="第1技術部";
-			break;
-		case "2":
-			this.depart="第2技術部";
-			break;
-		case "3":
-			this.depart="第3技術部";
-			break;
-		case "4":
-			this.depart="第4技術部";
-			break;
-		case "5":
-			this.depart="第5技術部";
-			break;
-		case "6":
-			this.depart="ソリューション技術部";
-			break;
-		}
+		this.depart=depart;
 	}
 
-	/**
-	 * 申請日のgetterメソッド
-	 * @return petition_ymd
-	 */
+/* 申請日(petition_ymd) getter */
 	public String getPetition_ymd() {
 		return petition_ymd;
 	}
 
-	/**
-	 * @param petition_ymd セットする petition_ymd
-	 */
+/* 申請日(petition_ymd) setter */
 	public void setPetition_ymd(String petition_ymd) {
 		this.petition_ymd = petition_ymd;
 	}
 
-	/**
-	 * @return attendance_startday
-	 */
+/* 対象日(開始)(attendance_startday) getter */
 	public String getAttendance_startday() {
 		return attendance_startday;
 	}
 
-	/**
-	 * @param attendance_startday セットする attendance_startday
-	 */
+/* 対象日(開始)(attendance_startday) setter */
 	public void setAttendance_startday(String attendance_startday) {
 		this.attendance_startday = attendance_startday;
 	}
 
-	/**
-	 * @return attendance_endday
-	 */
+/* 対象日(終了)(attendance_endday) getter */
 	public String getAttendance_endday() {
 		return attendance_endday;
 	}
 
-	/**
-	 * @param attendance_endday セットする attendance_endday
-	 */
+/* 対象日(終了)(attendance_endday) setter */
 	public void setAttendance_endday(String attendance_endday) {
 		this.attendance_endday = attendance_endday;
 	}
 
-	/**
-	 * @return attendance_starttime
-	 */
+
+/* 対象時間(開始)(attendance_starttime) getter */
 	public String getAttendance_starttime() {
 		return attendance_starttime;
 	}
 
-	/**
-	 * @param attendance_starttime セットする attendance_starttime
-	 */
+/* 対象時間(開始)(attendance_starttime) setter */
 	public void setAttendance_starttime(String attendance_starttime) {
 		this.attendance_starttime = attendance_starttime;
 	}
 
-	/**
-	 * @return attendance_endtime
-	 */
+/* 対象時間(終了)(attendance_endtime) getter */
 	public String getAttendance_endtime() {
 		return attendance_endtime;
 	}
 
-	/**
-	 * @param attendance_endtime セットする attendance_endtime
-	 */
+/* 対象時間(終了)(attendance_endtime) setter */
 	public void setAttendance_endtime(String attendance_endtime) {
 		this.attendance_endtime = attendance_endtime;
 	}
 
-	/**
-	 * @return notification_reason
-	 */
+/* 届出事由(notification_reason) getter */
 	public String getNotification_reason() {
 		return notification_reason;
 	}
 
-	/**
-	 * @param notification_reason セットする notification_reason
-	 */
+/* 届出事由(notification_reason) setter */
 	public void setNotification_reason(String notification_reason) {
 		this.notification_reason = notification_reason;
 	}
 
-	/**
-	 * @return vacation_division
-	 */
+/* 休暇区分(vacation_division) getter */
 	public String getVacation_division() {
 		return vacation_division;
 	}
 
-	/**
-	 * @param vacation_division セットする vacation_division
-	 */
+/* 休暇区分(vacation_division) setter */
 	public void setVacation_division(String vacation_division) {
 		this.vacation_division = vacation_division;
 	}
 
-	/**
-	 * @return sp_holiday_reason
-	 */
-	public String getSp_holiday_reason() {
-		return sp_holiday_reason;
-	}
-
-	/**
-	 * @param sp_holiday_reason セットする sp_holiday_reason
-	 */
-	public void setSp_holiday_reason(String sp_holiday_reason) {
-		this.sp_holiday_reason = sp_holiday_reason;
-	}
-
-	/**
-	 * @return absenteeism_reason
-	 */
-	public String getAbsenteeism_reason() {
-		return absenteeism_reason;
-	}
-
-	/**
-	 * @param absenteeism_reason セットする absenteeism_reason
-	 */
-	public void setAbsenteeism_reason(String absenteeism_reason) {
-		this.absenteeism_reason = absenteeism_reason;
-	}
-
-	/**
-	 * @return reason
-	 */
-	public String getReason() {
-		return reason;
-	}
-
-	/**
-	 * @param reason セットする reason
-	 */
-	public void setReason(String reason) {
-		this.reason = reason;
-	}
-
-	/**
-	 * メッセージ取得処理。
-	 * <p>
-	 * 勤怠届作成画面アクションフォームからメッセージを取得する。
-	 * </p>
-	 *
-	 * @return メッセージ
-	 */
-	public String getMessage() {
-		return message;
-	}
-
-	/**
-	 * メッセージ設定処理。
-	 * <p>
-	 * 勤怠届作成画面アクションフォームにメッセージを設定する。
-	 * </p>
-	 *
-	 * @param message メッセージ
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	/**
-	 * ボタン名取得処理。
-	 * <p>
-	 * 勤怠届作成画面アクションフォームからボタン名を取得する。
-	 * </p>
-	 *
-	 * @return ボタン名
-	 */
-	public String getButton() {
-
-		if(button.equals("ã­ã°ã¤ã³")){
-			button = "login";
-		}
-
-		return button;
-	}
-
-	/**
-	 * ボタン名設定処理。
-	 *
-	 * 勤怠届作成画面アクションフォームにボタン名を設定する。
-	 *
-	 * @param button ボタン名
-	 */
-	public void setButton(String button) {
-		this.button = button;
-	}
-
-	/**
-	 * @return transfer_day
-	 */
+/* 振替対象日(transfer_day) getter */
 	public String getTransfer_day() {
 		return transfer_day;
 	}
 
-	/**
-	 * @param transfer_day セットする transfer_day
-	 */
+/* 振替対象日(transfer_day) setter */
 	public void setTransfer_day(String transfer_day) {
 		this.transfer_day = transfer_day;
+	}
+
+/* 特休理由(sp_holiday_reason) getter */
+	public String getSp_holiday_reason() {
+		return sp_holiday_reason;
+	}
+
+/* 特休理由(sp_holiday_reason) setter */
+	public void setSp_holiday_reason(String sp_holiday_reason) {
+		this.sp_holiday_reason = sp_holiday_reason;
+	}
+
+/* 欠勤理由(absenteeism_reason) getter */
+	public String getAbsenteeism_reason() {
+		return absenteeism_reason;
+	}
+
+/* 欠勤理由(absenteeism_reason) setter */
+	public void setAbsenteeism_reason(String absenteeism_reason) {
+		this.absenteeism_reason = absenteeism_reason;
+	}
+
+/* 事由(reason) getter */
+	public String getReason() {
+		return reason;
+	}
+
+/* 事由(reason) setter */
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+/* メッセージ(message) getter */
+	public String getMessage() {
+		return message;
+	}
+
+/* メッセージ(message) setter */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+/* ボタン名(button) getter */
+	public String getButton() {
+		return button;
+	}
+
+/* ボタン名(button) setter */
+	public void setButton(String button) {
+		this.button = button;
 	}
 
 
