5138dd1c0a4524c9dc86ee52b32086611cf20ed7
kotaro_kawagoe <kotaro_kawagoe@Pro20150214022.procd-kama.local>
2020-09-16 11:06:49 +0900

    フォームクラスにフィールド変数を追加

--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/KinmuRecordAction.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/KinmuRecordAction.java
@@ -69,19 +69,27 @@ public class KinmuRecordAction extends Action{
 				forward="main";
 			} else if(button.equals("入力内容を保存")){
 				//31日分のインスタンスを保存
-				for(int i=1; i<=31; i++){
+//				for(int i=1; i<=31; i++){
 					//リクパラ用
 //					String holidayDivReq = "\"" + "holidayDiv" + String.valueOf(i) + "\"";
 
 					//リクエストパラメーターを取得
-					String holidayDiv = request.getParameter("holidayDiv");
-					String startTime = request.getParameter("startTime");
-					String endTime = request.getParameter("endTime");
-					String expectation = request.getParameter("expectation");
-					String breakTimeA = request.getParameter("breakTimeA");
-					String breakTimeB = request.getParameter("breakTimeB");
-					String vacationDiv = request.getParameter("vacationDiv");
-					String remark = request.getParameter("remark");
+					String holidayDiv1 = request.getParameter("holidayDiv1");
+					String holidayDiv2 = request.getParameter("holidayDiv2");
+					String startTime1 = request.getParameter("startTime1");
+					String startTime2 = request.getParameter("startTime2");
+					String endTime1 = request.getParameter("endTime1");
+					String endTime2 = request.getParameter("endTime2");
+					String expectation1 = request.getParameter("expectation1");
+					String expectation2 = request.getParameter("expectation2");
+					String breakTimeA1 = request.getParameter("breakTimeA1");
+					String breakTimeA2 = request.getParameter("breakTimeA2");
+					String breakTimeB1 = request.getParameter("breakTimeB1");
+					String breakTimeB2 = request.getParameter("breakTimeB2");
+					String vacationDiv1 = request.getParameter("vacationDiv1");
+					String vacationDiv2 = request.getParameter("vacationDiv2");
+					String remark1 = request.getParameter("remark1");
+					String remark2 = request.getParameter("remark2");
 //					String startTime = request.getParameter("startTime" + String.valueOf(i));
 //					String endTime = request.getParameter("endTime" + String.valueOf(i));
 //					String expectation = request.getParameter("expectation" + String.valueOf(i));
@@ -91,23 +99,45 @@ public class KinmuRecordAction extends Action{
 //					String remark = request.getParameter("remark" + String.valueOf(i));
 
 					StringBuffer sb = new StringBuffer();
-					sb.append(holidayDiv + startTime + endTime + expectation + breakTimeA + breakTimeB + vacationDiv + remark);
-					String query = sb.toString();
 
-					if(query.length()!=0){
+
+					sb.append(holidayDiv1 + startTime1 + endTime1 + expectation1 + breakTimeA1 + breakTimeB1 + vacationDiv1 + remark1);
+					String query1 = sb.toString();
+
+					if(query1.length()!=0){
 						//セッターでインスタンスのフィールド変数を更新
-						KRForm.setHolidayDiv(holidayDiv);
-						KRForm.setStartTime(startTime);
-						KRForm.setEndTime(endTime);
-						KRForm.setExpectation(expectation);
-						KRForm.setBreakTimeA(breakTimeA);
-						KRForm.setBreakTimeB(breakTimeB);
-						KRForm.setVacationDiv(vacationDiv);
-						KRForm.setRemark(remark);
+						KRForm.setHolidayDiv1(holidayDiv1);
+						KRForm.setStartTime1(startTime1);
+						KRForm.setEndTime1(endTime1);
+						KRForm.setExpectation1(expectation1);
+						KRForm.setBreakTimeA1(breakTimeA1);
+						KRForm.setBreakTimeB1(breakTimeB1);
+						KRForm.setVacationDiv1(vacationDiv1);
+						KRForm.setRemark1(remark1);
 						//インスタンスのフィールド変数が更新された状態で保存処理を実行
 						forward = save(KRForm);
 					}
-				}
+
+
+
+
+					sb.append(holidayDiv2 + startTime2 + endTime2 + expectation2 + breakTimeA2 + breakTimeB2 + vacationDiv2 + remark2);
+					String query2 = sb.toString();
+
+					if(query2.length()!=0){
+						//セッターでインスタンスのフィールド変数を更新
+						KRForm.setHolidayDiv2(holidayDiv2);
+						KRForm.setStartTime2(startTime2);
+						KRForm.setEndTime2(endTime2);
+						KRForm.setExpectation2(expectation2);
+						KRForm.setBreakTimeA2(breakTimeA2);
+						KRForm.setBreakTimeB2(breakTimeB2);
+						KRForm.setVacationDiv2(vacationDiv2);
+						KRForm.setRemark2(remark2);
+						//インスタンスのフィールド変数が更新された状態で保存処理を実行
+						forward = save(KRForm);
+					}
+//				}
 			}
 //			else if(button.equals("勤怠連絡入力")){
 //				forward="kintaimail";
