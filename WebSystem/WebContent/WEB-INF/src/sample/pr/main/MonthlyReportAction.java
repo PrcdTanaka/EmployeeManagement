7ea7dd08d6fc9230cda3a2335b6640ed5487344d
ryunosuke_sunaga <ryunosuke_sunaga@Pro20150214001.procd-kama.local>
2020-10-27 10:33:42 +0900

    a

--- WebSystem/WebContent/WEB-INF/src/sample/pr/main/MonthlyReportAction.java
+++ WebSystem/WebContent/WEB-INF/src/sample/pr/main/MonthlyReportAction.java
@@ -39,7 +39,7 @@ public class MonthlyReportAction extends Action {
 
 	public MonthlyReportAction() throws IOException {
 	}
-//qqqq
+
 	String button;
 
 	public ActionForward execute(ActionMapping map, ActionForm frm,
